3ef109fbe13fd3fe49e291ebce2b9b6cb0c08c92
Elena <elena@madetech.com>
2019-01-25 15:03:13 +0000

    basic translations

--- src/UseCases/ConvertTextToEmoji/ConverTextToEmoji.test.ts
+++ src/UseCases/ConvertTextToEmoji/ConverTextToEmoji.test.ts
@@ -1,6 +1,27 @@
 import Reacr from 'react';
+import Emojir from '.';
+
+it('should return a sensible default', () => {
+  const translator = new Emojir();
+  expect(translator.execute('')).toEqual('☠️');
+});
 
 it('should turn a into 😀', () => {
   const translator = new Emojir();
+  expect(translator.execute('a')).toEqual('😀');
+});
 
+it('should turn b into 🐝', () => {
+  const translator = new Emojir();
+  expect(translator.execute('b')).toEqual('🐝');
+});
+
+it('should turn c into 🌊', () => {
+  const translator = new Emojir();
+  expect(translator.execute('c')).toEqual('🌊');
+});
+
+it('should turn d into 🍆', () => {
+  const translator = new Emojir();
+  expect(translator.execute('d')).toEqual('🍆');
 });
---
+++ src/UseCases/ConvertTextToEmoji/index.ts
@@ -0,0 +1,12 @@
+export default class Emojir {
+  private tings: Map<string, string> = new Map<string, string>();
+  constructor() {
+    this.tings.set('a','😀');
+    this.tings.set('b','🐝');
+    this.tings.set('c','🌊');
+    this.tings.set('d','🍆');
+  }
+  execute(text: string): string {
+    return this.tings.get(text) || '☠️';
+  }
+}
